729810a33d20c0290cb06ba97588a2eee311b8e8
mullinix <james.mullinix@gmail.com>
2019-06-06 09:30:53 -0700

    cleanup run script / improve directory message

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SOURCE_DIR=$1
 if [ "$1" = "" ]; then
 	SOURCE_DIR=$(pwd)
 else
 	SOURCE_DIR=$1
 fi
-echo $SOURCE_DIR
+echo "Working directory set to: ${SOURCE_DIR}"
 docker run -it --rm \
+  --runtime=nvidia \
   -v "$SOURCE_DIR"/:/data \
   -w /data mullinix/nvidia-cuda-devel-gcc-gsl
 
